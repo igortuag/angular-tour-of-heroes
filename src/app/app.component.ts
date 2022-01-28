f22295bfc4027b6ed0f79af95ecea149ef950b69
Tuag <igortuag@gmail.com>
2022-01-28 13:54:21 -0300

    show hero details on the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,15 +5,15 @@ import { Component } from "@angular/core";
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"],
 })
-export class Hero {
-  id: number;
-  name: string;
-}
-
 export class AppComponent {
   title = "Tour of heroes";
   hero: Hero = {
     id: 1,
     name: "Windstorm",
   };
+}
+
+export class Hero {
+  id: number;
+  name: string;
 }
