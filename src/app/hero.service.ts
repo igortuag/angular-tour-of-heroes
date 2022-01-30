6931658bc575ca58929fa198de8225e9f7e20883
Tuag <igortuag@gmail.com>
2022-01-30 10:11:21 -0300

    add get hero return

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,9 +1,23 @@
+import { Hero } from './app.component';
 import { Injectable } from '@angular/core';
 
 @Injectable()
 export class HeroService {
 
-  getHeroes(): void { }
+  getHeroes(): Hero[] {
+    return [
+      { id: 11, name: "Mr. Nice" },
+      { id: 12, name: "Narco" },
+      { id: 13, name: "Bombasto" },
+      { id: 14, name: "Celeritas" },
+      { id: 15, name: "Magneta" },
+      { id: 16, name: "RubberMan" },
+      { id: 17, name: "Dynama" },
+      { id: 18, name: "Dr IQ" },
+      { id: 19, name: "Magma" },
+      { id: 20, name: "Tornado" },
+    ];
+  }
 
   constructor() { }
 
