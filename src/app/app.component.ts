441d69773eaa6455b44d20a7f7b0b3e73f87814d
Tuag <igortuag@gmail.com>
2022-01-30 10:18:28 -0300

    add heros ass a promisse to simulate api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,7 +25,7 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.heros = this.heroService.getHeroes();
+    this.heroService.getHeroes().then(heros => this.heros = heros);
   }
 }
 
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,24 +1,19 @@
-import { Hero } from './app.component';
 import { Injectable } from '@angular/core';
+import { Hero } from './hero-detail/hero';
+import { HEROES } from './mock-heroes';
+
+
 
 @Injectable()
 export class HeroService {
 
-  getHeroes(): Hero[] {
-    return [
-      { id: 11, name: "Mr. Nice" },
-      { id: 12, name: "Narco" },
-      { id: 13, name: "Bombasto" },
-      { id: 14, name: "Celeritas" },
-      { id: 15, name: "Magneta" },
-      { id: 16, name: "RubberMan" },
-      { id: 17, name: "Dynama" },
-      { id: 18, name: "Dr IQ" },
-      { id: 19, name: "Magma" },
-      { id: 20, name: "Tornado" },
-    ];
+
+  getHeroes(): Promise<Hero[]> {
+    return Promise.resolve(HEROES)
   }
 
   constructor() { }
 
 }
+
+
